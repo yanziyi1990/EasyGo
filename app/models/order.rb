32ab6df3ccec4fbb2a44cc4aeb21ef7255816abb
ZIYI YAN <ziyi@tribe20.com>
2016-11-22 20:49:40 -0800

    feat: add orders related methods

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,12 @@
 class Order < ActiveRecord::Base
-  def delete_closed_order
+  def self.delete_closed_order
     Order.where(status:'Shipped').destroy_all
   end
+
+  def self.print_pick_list
+    unprocess_order=Order.where(status:'unprocessed')
+    location_hash=Hash.new([])
+
+  end
+
 end
