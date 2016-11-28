81ad5622812f6c3c430f7d5a5af65d6a150f6855
ZIYI YAN <ziyi@tribe20.com>
2016-11-28 13:43:44 -0800

    fix: edit page error

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -70,6 +70,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.fetch(:product, {})
+      params.require(:product).permit(:sku, :product_name, :image_url1, :image_url2, :image_url3, :product_description, :category, :color, :vendor)
     end
 end
